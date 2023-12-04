e7e093dbfdd79b4f4613b34552daa48e4c7a3278
Carlos Cueto <crcueto25@gmail.com>
2023-12-04 13:40:30 +0200

    Update scan_demo node
    add plotting using matplot lib by flushing the window

--- ros2_ws/src/scan_demo/scan_demo/subscriber.py
+++ ros2_ws/src/scan_demo/scan_demo/subscriber.py
@@ -8,12 +8,17 @@ from rclpy.node import Node
 class MinimalSubscriber(Node):
     def __init__(self):
         super().__init__("minimal_subscriber")
+        # Create the subscriber. This subscriber will receive a Scan message and the window for plotting
         self.subscription = self.create_subscription(
             Scan, "range", self.listener_callback, 10
         )
         self.subscription  # prevent unused variable warning
         # Variable to save the data in npz format
         self.data_npz = []
+        # Initialize the matplotlib window
+        self.fig, self.ax = plt.subplots()
+        (self.line,) = self.ax.plot(np.random.randn(1536))
+        plt.show(block=False)
 
     def listener_callback(self, msg):
         id = msg.id
@@ -21,16 +26,32 @@ class MinimalSubscriber(Node):
         width = msg.width
         step = msg.step
         data = msg.data
-        # Reshape the data to a 2D array
+        # Reshape the data to a 2D array of 512, whatever the width is
         data = np.array(data).reshape(height, width)
         # Print the data information
         self.get_logger().info(f"ID: {id}")
         self.get_logger().info(f"height: {height}, width: {width}, step: {step}")
         self.get_logger().info(f"size: {len(data)}")
+        # Normalize the data
+        data = data / np.max(data)
+
         # Plot the data as an image
         # fig, ax = plt.subplots()
         # ax.imshow(data, cmap="gray")
         # plt.show()
+        # Itereate over the data as each row is one profile
+        for i in range(len(data)):
+            # print(np.array(data[i]))
+            # Update the plot
+            self.line.set_ydata(np.array(data[i]))
+            self.ax.relim()
+            self.ax.autoscale_view()
+            self.ax.draw_artist(self.ax.patch)
+            self.ax.draw_artist(self.line)
+            self.fig.canvas.update()
+            self.fig.canvas.flush_events()
+            # Wait 0.01 seconds to view the plot
+            plt.pause(0.01)
         # Save the data
         self.data_npz.append(data)
         # print(len(self.data_npz))
@@ -46,7 +67,7 @@ def main(args=None):
         rclpy.spin(minimal_subscriber)
     except KeyboardInterrupt:
         # Save the data in npz format
-        np.savez_compressed("data_coin", minimal_subscriber.data_npz)
+        # np.savez_compressed("data_136pc", minimal_subscriber.data_npz)
         # Destroy the node
         minimal_subscriber.destroy_node()
         # Shutdown the ROS client library for Python
