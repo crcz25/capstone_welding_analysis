b2b4ce2b1834508bbf3f867e8d65c56c69a51a98
Carlos Cueto <crcueto25@gmail.com>
2024-02-27 18:58:23 +0200

    add docs main app gui

--- python_gui_py/App.py
+++ python_gui_py/App.py
@@ -19,6 +19,28 @@ ctk.set_default_color_theme("dark-blue")
 
 # --------------------------------------------------------GUI BASE--------------------------------------------------------#
 class App(ctk.CTk):
+    """
+    The main application class for the GUI.
+
+    Attributes:
+    - menu_frame (MenuFrame): The frame for the menu options.
+    - plot_frame (PlotFrame): The frame for displaying the plot.
+    - info_frame (InfoFrame): The frame for displaying information.
+    - plot_control_frame (PlotControlFrame): The frame for controlling the plot.
+    - settings_frame (SettingsFrame): The frame for the settings.
+
+    Methods:
+    - __init__(): Initializes the App class.
+    - change_appearance_mode_event(new_appearance_mode: str): Changes the appearance mode of the GUI.
+    - change_scaling_event(new_scaling: str): Changes the scaling of the GUI.
+    - change_button_text(button, text_1, text_2): Changes the text of a button.
+    - change_console_text(text, tags=None): Logs text to the UI console.
+    - menu_button(frame_type): Handles button clicks in the menu.
+    - show_camera_frames(): Shows the frames related to camera settings.
+    - show_default_frames(): Shows the default frames.
+    - reset_imported_files(): Resets the imported files.
+    - import_files(): Imports files into the application.
+    """
     def __init__(self):
         super().__init__()
 
@@ -66,6 +88,15 @@ class App(ctk.CTk):
 
     # --------------------------------------------------------FUNCTIONALITY--------------------------------------------------------#
     def change_appearance_mode_event(self, new_appearance_mode: str):
+        """
+        Change the appearance mode of the application.
+
+        Args:
+            new_appearance_mode (str): The new appearance mode to set.
+
+        Returns:
+            None
+        """
         ctk.set_appearance_mode(new_appearance_mode)
         self.show_default_frames()
         self.change_console_text(
@@ -73,11 +104,28 @@ class App(ctk.CTk):
         )  # Make sure that text colors change when theme is changed
 
     def change_scaling_event(self, new_scaling: str):
+        """
+        Change the scaling of the widget and show default frames.
+
+        Args:
+            new_scaling (str): The new scaling value as a string with a percentage symbol (%).
+
+        Returns:
+            None
+        """
         new_scaling_float = int(new_scaling.replace("%", "")) / 100
         ctk.set_widget_scaling(new_scaling_float)
         self.show_default_frames()
 
     def change_button_text(self, button, text_1, text_2):
+        """
+        Changes the text of a button based on its current state.
+
+        Parameters:
+        button (tkinter.Button): The button to change the text of.
+        text_1 (str): The first text to be displayed on the button.
+        text_2 (str): The second text to be displayed on the button.
+        """
         current_state = button.cget("text")
         new_state = text_2 if current_state == text_1 else text_1
         # Check if it is time to start or stop the scan
@@ -96,12 +144,11 @@ class App(ctk.CTk):
 
     def change_console_text(self, text, tags=None):
         """
-        Function to log text to the UI console.
+        Function to log text to the UI console with different tags.
 
         Parameters:
         - text (text to be shown).
         - tag (INFO: white, ERROR: red, SUCCESS: green)
-
         """
         print(text)  # Kept this to print to terminal
 
@@ -134,24 +181,44 @@ class App(ctk.CTk):
         self.plot_control_frame.console_entry.see("end")
 
     def menu_button(self, frame_type):
+        """
+        Handles the menu button click event and displays the corresponding frames.
+
+        Args:
+            frame_type (str): The type of frame to be displayed.
+
+        Returns:
+            None
+        """
         if frame_type == "home":
             self.show_default_frames()
         elif frame_type == "settings":
             self.show_camera_frames()
 
     def show_camera_frames(self):
+        """
+        Hides the info frame, plot control frame, and plot frame, and shows the settings frame.
+        """
         self.info_frame.grid_remove()
         self.plot_control_frame.grid_remove()
         self.plot_frame.grid_remove()
         self.settings_frame.grid()
 
     def show_default_frames(self):
+        """
+        Show the default frames in the GUI.
+
+        This method hides the settings frame and shows the info frame, plot frame, and plot control frame.
+        """
         self.settings_frame.grid_remove()
         self.info_frame.grid()
         self.plot_frame.grid()
         self.plot_control_frame.grid()
 
     def reset_imported_files(self):
+        """
+        Resets the imported files and associated data to their initial state.
+        """
         self.range_file = None
         self.timestamp_file = None
         self.range_data = np.array([])
@@ -163,6 +230,16 @@ class App(ctk.CTk):
         self.update_info_frame()
 
     def import_files(self):
+        """
+        Import files from a selected directory.
+
+        This method prompts the user to select a directory and imports the files with the extensions .npy and .csv
+        from that directory. It performs various checks and validations on the imported files and updates the
+        necessary data structures and UI elements accordingly.
+
+        Returns:
+            None
+        """
         # If there is a scan already imported, replace it with the new one and clean the plot frame
         if self.range_file is not None and self.timestamp_file is not None:
             self.reset_imported_files()
@@ -273,7 +350,12 @@ class App(ctk.CTk):
         self.update_info_frame()
 
     def timestamp_formatter(self):
-        # Format csv file timestamps back to human readable for the UI
+        """
+        Formats the csv file timestamps back to human readable format for the UI.
+
+        Returns:
+            None
+        """
         self.formatted_timestamps = []
         for timestamp in self.timestamp_data:
             formatted_timestamp = datetime.fromtimestamp(timestamp / 1e9).strftime(
@@ -282,6 +364,12 @@ class App(ctk.CTk):
             self.formatted_timestamps.append(formatted_timestamp)
 
     def update_info_frame(self, new_limits=None):
+        """
+        Updates the information frame with relevant data.
+
+        Args:
+            new_limits (list, optional): New limits for the cursor. Defaults to None.
+        """
         # Check if the data is loaded
         if self.range_file is not None and self.timestamp_file is not None:
             # Enable the textbox
@@ -340,6 +428,16 @@ class App(ctk.CTk):
             self.change_console_text("Data is not loaded", "ERROR")
 
     def change_plot(self, change, profile=0):
+        """
+        Change the plot based on the given change and profile.
+
+        Parameters:
+        - change (int): The amount to change the current profile by.
+        - profile (int): The profile to display on the plot.
+
+        Returns:
+        None
+        """
         # Check if the data is loaded
         if self.range_data.size > 0 and self.timestamp_data.size > 0:
             self.current_profile += change
@@ -372,6 +470,14 @@ class App(ctk.CTk):
         self.plot_control_frame.take_stop_scan()
 
     def start_scan(self):
+        """
+        Starts the scanning process.
+
+        If scanning is not already in progress, it sets the scanning_in_progress flag to True,
+        creates a ROSNodeThread instance, starts the thread, and updates the console text to indicate
+        that the scan has started. If scanning is already in progress, it updates the console text
+        to indicate that the scan did not start.
+        """
         if not self.scanning_in_progress:
             self.scanning_in_progress = True
             self.ros_node_thread = ROSNodeThread()
@@ -381,6 +487,12 @@ class App(ctk.CTk):
             self.change_console_text("Scan did not start", "INFORMATION")
 
     def stop_scan(self):
+        """
+        Stops the scanning process and saves the data.
+
+        If scanning is in progress, it stops the node, saves the data to the specified directory,
+        and updates the console text accordingly.
+        """
         if self.scanning_in_progress:
             # Stop the node
             self.scanning_in_progress = False
